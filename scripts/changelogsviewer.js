dfab13093b24053d6e5cb240b6887e61bf9e493b
Jalenzz <126164913+Jalenzzz@users.noreply.github.com>
2024-08-12 01:02:32 +0300

    7B visits website update

--- scripts/changelogsviewer.js
+++ scripts/changelogsviewer.js
@@ -1,18 +1,21 @@
+let searchTimeout;
 document.addEventListener("DOMContentLoaded", () => {
-  console.log("DOM fully loaded and parsed");
+  // console.log("DOM fully loaded and parsed");
 
   const contentElement = document.getElementById("content");
-  const paginationElement = document.getElementById("pagination");
+  const pageSelectElement = document.getElementById("pageSelect");
+  const pageInfoElement = document.getElementById("pageInfo");
   const imageElement = document.querySelector(".sidebar-image");
-  const body = document.body;
   const themeToggle = document.getElementById("theme-toggle");
   const currentTheme = localStorage.getItem("theme") || "light";
+  const searchForm = document.getElementById("search-form");
+  const body = document.body;
   const itemsPerPage = 1;
   let currentPage = 1;
   let totalPages;
   let changelogItems = [];
 
-  console.log("Variables initialized");
+  // console.log("Variables initialized");
 
   const logoImage = document.getElementById("logo");
   let hasLoggedPlayError = false;
@@ -63,10 +66,10 @@ document.addEventListener("DOMContentLoaded", () => {
     { threshold: 0.1 }
   );
 
-  console.log("Observers set up");
+  // console.log("Observers set up");
 
   window.updateSidebarImage = (pageNumber) => {
-    console.log("Updating sidebar image for page:", pageNumber);
+    // console.log("Updating sidebar image for page:", pageNumber);
     fetch("data/sidebar_images.json")
       .then((response) => {
         if (!response.ok) {
@@ -79,7 +82,7 @@ document.addEventListener("DOMContentLoaded", () => {
         const imageUrl = image
           ? image.url
           : "https://res.cloudinary.com/dt6ym6zrw/image/upload/v1722811458/16ff8b3a-92de-43d6-ae92-094ad17f74a5.png";
-        console.log("Selected image URL:", imageUrl);
+        // console.log("Selected image URL:", imageUrl);
 
         const imageElement = document.querySelector(".sidebar-image");
         if (imageElement) {
@@ -97,7 +100,7 @@ document.addEventListener("DOMContentLoaded", () => {
   // Initialize with the first image
   updateSidebarImage(1);
 
-  console.log("Fetching changelog data...");
+  // console.log("Fetching changelog data...");
   fetch(
     "https://raw.githubusercontent.com/JBChangelogs/JailbreakChangelogs/main/changelogs/2-8-24.txt",
     {
@@ -106,7 +109,6 @@ document.addEventListener("DOMContentLoaded", () => {
   )
     .then((response) => response.text())
     .then((data) => {
-      console.log("Changelog data received, processing...");
       const lines = data.split("\n");
       let currentItem = "";
       let currentDate = "";
@@ -118,23 +120,23 @@ document.addEventListener("DOMContentLoaded", () => {
             currentItem = "";
           }
           currentDate = line.substring(2);
-          currentItem = `<h1>${line.substring(2)}</h1>`;
+          currentItem = `<h1>${wrapMentions(line.substring(2))}</h1>`;
         } else if (line.startsWith("- - ")) {
-          currentItem += `<p><span class="bullet">•</span> <span class="sub-bullet">•</span> ${line.substring(
-            4
+          currentItem += `<p><span class="bullet">•</span> <span class="sub-bullet">•</span> ${wrapMentions(
+            line.substring(4)
           )}</p>`;
         } else if (line.startsWith("## ")) {
-          currentItem += `<h2>${line.substring(3)}</h2>`;
+          currentItem += `<h2>${wrapMentions(line.substring(3))}</h2>`;
         } else if (line.startsWith("- ")) {
-          currentItem += `<p><span class="bullet">•</span> ${line.substring(
-            2
+          currentItem += `<p><span class="bullet">•</span> ${wrapMentions(
+            line.substring(2)
           )}</p>`;
         } else if (line.startsWith("- - - ")) {
-          currentItem += `<p><span class="bullet">•</span> <span class="sub-bullet">•</span>  ${line.substring(
-            4
+          currentItem += `<p><span class="bullet">•</span> <span class="sub-bullet">•</span> ${wrapMentions(
+            line.substring(4)
           )}</p>`;
         } else {
-          currentItem += `<p>${line}</p>`;
+          currentItem += `<p>${wrapMentions(line)}</p>`;
         }
       });
 
@@ -145,17 +147,28 @@ document.addEventListener("DOMContentLoaded", () => {
       changelogItems = changelogItems.reverse();
       totalPages = Math.ceil(changelogItems.length / itemsPerPage) || 1;
 
-      console.log("Changelog processed, rendering...");
+      // Load the saved page or default to 1
+      currentPage = loadSavedPage();
+
+      // console.log("Changelog processed, rendering...");
       renderChangelogs();
       renderPagination();
       adjustSelectWidth();
-      console.log("Initial render complete");
+      updateSidebarImage(currentPage);
+      // console.log("Initial render complete");
     })
     .catch((error) => {
       console.error("Error fetching the changelog:", error);
       contentElement.innerHTML =
         "<p>Error loading changelog. Please try again later.</p>";
     });
+
+  function wrapMentions(text) {
+    return text.replace(
+      /@(\w+)/g,
+      '<span class="mention"><span class="at">@</span>$1</span>'
+    );
+  }
 
   function renderChangelogs() {
     const startIndex = (currentPage - 1) * itemsPerPage;
@@ -171,18 +184,17 @@ document.addEventListener("DOMContentLoaded", () => {
   }
 
   function renderPagination() {
-    let paginationHtml = "";
-
-    paginationHtml += `<select id="pageSelect" class="pagination-select" onchange="goToPage(this.value)">`;
+    pageSelectElement.innerHTML = "";
     for (let i = 1; i <= totalPages; i++) {
       const date = changelogItems[i - 1].date;
-      paginationHtml += `<option value="${i}" ${
-        i === currentPage ? "selected" : ""
-      }>${i} - ${date}</option>`;
+      const option = document.createElement("option");
+      option.value = i;
+      option.textContent = truncateText(`${i} - ${date}`, 30);
+      option.title = `${i} - ${date}`;
+      option.selected = i === currentPage;
+      pageSelectElement.appendChild(option);
     }
-    paginationHtml += `</select>`;
-
-    paginationElement.innerHTML = paginationHtml;
+    pageInfoElement.textContent = `${currentPage} / ${totalPages}`;
   }
 
   function adjustSelectWidth() {
@@ -192,42 +204,48 @@ document.addEventListener("DOMContentLoaded", () => {
     tempSpan.style.visibility = "hidden";
     tempSpan.style.position = "absolute";
     tempSpan.style.whiteSpace = "nowrap";
-    tempSpan.innerHTML = selectedOption.text;
+    tempSpan.innerHTML = selectedOption.textContent; // Use truncated text
     document.body.appendChild(tempSpan);
     const optionWidth = tempSpan.offsetWidth;
     document.body.removeChild(tempSpan);
 
-    selectElement.style.width = `${optionWidth + 20}px`;
+    selectElement.style.width = `${optionWidth + 100}px`; // Increased padding to account for ellipsis
   }
 
-  function performSearch() {
-    console.log("Performing search");
+  function performSearch(shouldScroll = false) {
+    // console.log("Performing search");
     const searchTerm = searchInput.value.toLowerCase();
-    console.log("Search term:", searchTerm);
+    // console.log("Search term:", searchTerm);
+    try {
+      const filteredItems = changelogItems.filter(
+        (item) =>
+          item.content.toLowerCase().includes(searchTerm) ||
+          item.date.toLowerCase().includes(searchTerm)
+      );
 
-    if (searchTerm.length < 3) {
-      console.log("Search term too short");
-      searchResultsContainer.innerHTML =
-        "<div class='search-results-content'><p>Please enter at least 3 characters to search.</p></div>";
+      // console.log("Filtered items:", filteredItems);
+
+      if (filteredItems.length > 0) {
+        renderSearchResults(filteredItems);
+      } else {
+        searchResultsContainer.innerHTML =
+          "<div class='search-results-content'><p>No results found.</p></div>";
+      }
       searchResultsContainer.style.display = "block";
-      return;
-    }
 
-    const filteredItems = changelogItems.filter(
-      (item) =>
-        item.content.toLowerCase().includes(searchTerm) ||
-        item.date.toLowerCase().includes(searchTerm)
-    );
-
-    console.log("Filtered items:", filteredItems);
-
-    if (filteredItems.length > 0) {
-      renderSearchResults(filteredItems);
-    } else {
+      if (shouldScroll) {
+        scrollToSearchResults();
+      }
+    } catch (error) {
+      console.error("Error in performSearch:", error);
       searchResultsContainer.innerHTML =
-        "<div class='search-results-content'><p>No results found.</p></div>";
+        "<div class='search-results-content'><p>An error occurred while searching. Please try again.</p></div>";
+      searchResultsContainer.style.display = "block";
+
+      if (shouldScroll) {
+        scrollToSearchResults();
+      }
     }
-    searchResultsContainer.style.display = "block";
   }
 
   function clearSearch() {
@@ -235,8 +253,95 @@ document.addEventListener("DOMContentLoaded", () => {
     searchResultsContainer.style.display = "none";
   }
 
+  function scrollToSearchResults() {
+    if (window.innerWidth <= 768) {
+      // Adjust this value based on your mobile breakpoint
+      setTimeout(() => {
+        const searchResultsContainer = document.getElementById("searchResults");
+        if (
+          searchResultsContainer &&
+          searchResultsContainer.style.display !== "none"
+        ) {
+          searchResultsContainer.scrollIntoView({
+            behavior: "smooth",
+            block: "start",
+          });
+        }
+      }, 300); // 300ms delay, adjust as needed
+    }
+  }
+
+  function truncateText(text, maxLength) {
+    if (text.length <= maxLength) return text;
+    return text.substring(0, maxLength - 3) + "...";
+  }
+
+  let currentFocusIndex = -1;
+
+  function handleArrowKeys(e) {
+    const results = document.querySelectorAll(".search-result-item");
+    if (results.length === 0) return;
+
+    if (e.key === "ArrowDown") {
+      e.preventDefault();
+      currentFocusIndex = (currentFocusIndex + 1) % results.length;
+    } else if (e.key === "ArrowUp") {
+      e.preventDefault();
+      currentFocusIndex =
+        (currentFocusIndex - 1 + results.length) % results.length;
+    } else if (e.key === "Enter" && currentFocusIndex !== -1) {
+      e.preventDefault();
+      selectSearchResult(results[currentFocusIndex]);
+      return;
+    } else {
+      return;
+    }
+
+    updateFocus(results);
+  }
+
+  function updateFocus(results) {
+    results.forEach((result, index) => {
+      if (index === currentFocusIndex) {
+        result.classList.add("focused");
+        result.scrollIntoView({ behavior: "smooth", block: "nearest" });
+      } else {
+        result.classList.remove("focused");
+      }
+    });
+  }
+
+  function selectSearchResult(result) {
+    const pageNumber = parseInt(result.dataset.page);
+    if (pageNumber > 0) {
+      goToPage(pageNumber);
+      clearSearch();
+      scrollToSearchResults();
+    }
+  }
+
+  document.addEventListener("keydown", handleArrowKeys);
+
+  // Add click event listeners to search results
+  document
+    .getElementById("searchResults")
+    .addEventListener("click", function (e) {
+      const clickedResult = e.target.closest(".search-result-item");
+      if (clickedResult) {
+        selectSearchResult(clickedResult);
+      }
+    });
+
+  function saveCurrentPage(page) {
+    localStorage.setItem("currentChangelogPage", page);
+  }
+
+  function loadSavedPage() {
+    return parseInt(localStorage.getItem("currentChangelogPage")) || 1;
+  }
+
   function renderSearchResults(results) {
-    console.log("Rendering search results:", results);
+    // console.log("Rendering search results:", results);
     let resultsHtml = "<div class='search-results-content'>";
     results.forEach((item) => {
       resultsHtml += `
@@ -244,7 +349,8 @@ document.addEventListener("DOMContentLoaded", () => {
           changelogItems.indexOf(item) + 1
         }">
           <strong>${item.date}</strong>
-          <p>${item.content.replace(/<[^>]*>/g, "").substring(0, 100)}...</p>
+          <p>${truncateText(item.content.replace(/<[^>]*>/g, ""), 100)}</p>
+
         </div>
       `;
     });
@@ -271,49 +377,82 @@ document.addEventListener("DOMContentLoaded", () => {
     renderPagination();
     adjustSelectWidth();
     updateSidebarImage(currentPage);
+    clearSearch();
+    saveCurrentPage(currentPage);
   };
+
   body.classList.toggle("dark-theme", currentTheme === "dark");
+
+  // Function to update theme
+  function updateTheme(theme) {
+    body.classList.toggle("dark-theme", theme === "dark");
+    localStorage.setItem("theme", theme);
+    updateThemeIcon();
+  }
 
   // Function to update theme icon
   function updateThemeIcon() {
     const isDark = body.classList.contains("dark-theme");
-    const iconName = isDark ? "sun" : "moon";
-    themeToggle.innerHTML = `<i data-lucide="${iconName}"></i>`;
-    lucide.createIcons();
+    const sunIcon = themeToggle.querySelector(".light-icon");
+    const moonIcon = themeToggle.querySelector(".dark-icon");
+
+    if (isDark) {
+      sunIcon.style.display = "block";
+      moonIcon.style.display = "none";
+    } else {
+      sunIcon.style.display = "none";
+      moonIcon.style.display = "block";
+    }
   }
 
-  const searchInput = document.getElementById("search-input");
-  const clearSearchButton = document.getElementById("clear-search");
-  const searchResultsContainer = document.getElementById("search-results");
+  // Set initial theme
+  updateTheme(currentTheme);
+
+  // Theme toggle event listener
+  themeToggle.addEventListener("click", () => {
+    const newTheme = body.classList.contains("dark-theme") ? "light" : "dark";
+    updateTheme(newTheme);
+  });
+
+  const searchInput = document.getElementById("searchInput");
+  const searchButton = document.getElementById("searchButton");
+  const clearSearchButton = document.getElementById("clearSearch");
+  const searchResultsContainer = document.getElementById("searchResults");
 
   let searchTimeout;
-
   searchInput.addEventListener("input", () => {
     clearTimeout(searchTimeout);
     searchTimeout = setTimeout(() => {
-      if (searchInput.value.length >= 3) {
-        performSearch();
-      } else if (searchInput.value.length === 0) {
-        clearSearch();
+      if (searchInput.value.length > 0) {
+        performSearch(false); // Pass false to indicate not to scroll
       } else {
-        searchResultsContainer.innerHTML =
-          "<div class='search-results-content'><p>Please enter at least 3 characters to search.</p></div>";
-        searchResultsContainer.style.display = "block";
+        clearSearch();
       }
-    }, 300); // Wait for 300ms after the user stops typing
+    }, 150);
   });
 
+  searchButton.addEventListener("click", performSearch);
   clearSearchButton.addEventListener("click", clearSearch);
 
-  // Initial icon update
-  updateThemeIcon();
-
-  themeToggle.addEventListener("click", () => {
-    body.classList.toggle("dark-theme");
-    const newTheme = body.classList.contains("dark-theme") ? "dark" : "light";
-    localStorage.setItem("theme", newTheme);
-    updateThemeIcon();
+  searchForm.addEventListener("submit", function (e) {
+    e.preventDefault();
+    performSearch(true); // Pass true to indicate scrolling
+    searchInput.blur();
   });
 
-  console.log("Script execution completed");
+  searchButton.addEventListener("click", function (e) {
+    e.preventDefault();
+    performSearch(true); // Pass true to indicate scrolling
+    searchInput.blur();
+  });
+
+  searchInput.addEventListener("search", function (e) {
+    performSearch(true);
+    searchInput.blur(); // Dismiss keyboard
+  });
+
+  pageSelectElement.addEventListener("change", (e) => {
+    goToPage(e.target.value);
+  });
+  // console.log("Script execution completed");
 });
