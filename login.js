2014f6778a52837680a2e2b8a2a6d525aebf7a1a
v3kmmw <v3kmww@gmail.com>
2024-09-30 02:38:16 -0500

    Add files via upload

---
+++ login.js
@@ -0,0 +1,37 @@
+$(document).ready(function () {
+  const OauthRedirect =
+    "https://discord.com/oauth2/authorize?client_id=1281308669299920907&response_type=code&redirect_uri=https%3A%2F%2Fjailbreakchangelogs.xyz%2Flogin&scope=identify";
+  const DiscordLoginButton = document.getElementById("login-button");
+  DiscordLoginButton.addEventListener("click", () => {
+    console.log("Redirecting to Discord OAuth...");
+    window.location.href = OauthRedirect;
+  });
+  if (window.location.search.includes("code=")) {
+    const code = new URLSearchParams(window.location.search).get("code");
+    fetch("https://api.jailbreakchangelogs.xyz/auth?code=" + code, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+    })
+      .then((response) => response.json())
+      .then((userData) => {
+        const avatarURL = `https://cdn.discordapp.com/avatars/${userData.id}/${userData.avatar}.png`;
+        sessionStorage.removeItem("token");
+        sessionStorage.removeItem("user");
+        sessionStorage.removeItem("avatar");
+        sessionStorage.removeItem("userid");
+        sessionStorage.setItem("token", userData.token);
+        sessionStorage.setItem("user", JSON.stringify(userData));
+        sessionStorage.setItem("avatar", avatarURL);
+        sessionStorage.setItem("userid", userData.id);
+        redirect = localStorage.getItem("redirectAfterLogin");
+        if (redirect === null) {
+          window.location.href = "/";
+        } else {
+          window.location.href = redirect;
+          localStorage.removeItem("redirectAfterLogin");
+        }
+      });
+  }
+});
