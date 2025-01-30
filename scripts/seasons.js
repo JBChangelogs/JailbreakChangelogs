a47d6d9eb102f1148d11fbd8ba5267f917955ed9
Jalenzz <126164913+Jalenzzz@users.noreply.github.com>
2025-01-30 16:07:52 +0300

    gosh i hate this icon revamp

--- scripts/seasons.js
+++ scripts/seasons.js
@@ -168,7 +168,10 @@ $(document).ready(function () {
                   ? `<p class="season-description-text">${seasonData.description}</p>`
                   : `
                       <div class="no-description">
-                          <i class="bi bi-info-circle text-muted mb-2" style="font-size: 2rem;"></i>
+                          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24">
+	<rect width="24" height="24" fill="none" />
+	<path fill="currentColor" d="M11 9h2V7h-2m1 13c-4.41 0-8-3.59-8-8s3.59-8 8-8s8 3.59 8 8s-3.59 8-8 8m0-18A10 10 0 0 0 2 12a10 10 0 0 0 10 10a10 10 0 0 0 10-10A10 10 0 0 0 12 2m-1 15h2v-6h-2z" />
+</svg>
                           <p class="text-muted">No description available.</p>
                       </div>`
               }
@@ -199,7 +202,10 @@ $(document).ready(function () {
                     ? `<p class="season-description-text">${seasonData.description}</p>`
                     : `
                         <div class="no-description">
-                            <i class="bi bi-info-circle text-muted mb-2" style="font-size: 2rem;"></i>
+                           <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24">
+	<rect width="24" height="24" fill="none" />
+	<path fill="currentColor" d="M11 9h2V7h-2m1 13c-4.41 0-8-3.59-8-8s3.59-8 8-8s8 3.59 8 8s-3.59 8-8 8m0-18A10 10 0 0 0 2 12a10 10 0 0 0 10 10a10 10 0 0 0 10-10A10 10 0 0 0 12 2m-1 15h2v-6h-2z" />
+</svg>
                             <p class="text-muted">No description available.</p>
                         </div>`
                 }
