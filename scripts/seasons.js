e23d82bf45bd7012f3090623a8b71c7321998245
Jalenzz <126164913+Jalenzzz@users.noreply.github.com>
2025-04-02 22:41:26 +0300

    season 26

--- scripts/seasons.js
+++ scripts/seasons.js
@@ -616,7 +616,7 @@ document.addEventListener("DOMContentLoaded", function () {
             <div class="season-countdown">
               <h3 class="countdown-title">Season ${nextSeason.season} / ${nextSeason.title} submissions have closed</h3>
               <div class="submission-notice" style="padding-top: 0;">
-                <a href="https://www.reddit.com/r/JailbreakCreations/comments/1ij9taa/season_${nextSeason.season}_entries_pixel_arcade/?sort=new" 
+                <a href="https://www.reddit.com/r/JailbreakCreations/comments/1jptlh2/season_26_entries_jungle_adventure/?sort=new" 
                    target="_blank" 
                    rel="noopener noreferrer" 
                    class="btn btn-sm btn-outline-info">
@@ -638,7 +638,7 @@ document.addEventListener("DOMContentLoaded", function () {
           <div class="season-countdown">
             <h3 class="countdown-title">Season ${nextSeason.season} / ${nextSeason.title} submissions have closed</h3>
             <div class="submission-notice" style="padding-top: 0;">
-              <a href="https://www.reddit.com/r/JailbreakCreations/comments/1ij9taa/season_${nextSeason.season}_entries_pixel_arcade/?sort=new" 
+              <a href="https://www.reddit.com/r/JailbreakCreations/comments/1jptlh2/season_26_entries_jungle_adventure/?sort=new" 
                  target="_blank" 
                  rel="noopener noreferrer" 
                  class="btn btn-sm btn-outline-info">
@@ -672,7 +672,7 @@ document.addEventListener("DOMContentLoaded", function () {
       isNextSeasonCountdown && isSubmissionsOpen
         ? `
       <div class="submission-notice">
-        <a href="https://www.reddit.com/r/JailbreakCreations/comments/1ij9taa/season_25_entries_pixel_arcade/?sort=new" 
+        <a href="https://www.reddit.com/r/JailbreakCreations/comments/1jptlh2/season_26_entries_jungle_adventure/?sort=new" 
            target="_blank" 
            rel="noopener noreferrer" 
            class="btn btn-sm btn-outline-info">
