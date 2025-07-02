b564d2eda7db48dff6b6e1c188fede3d33a81f50
Jalenzz <126164913+Jalenzzz@users.noreply.github.com>
2025-07-03 02:17:02 +0300

    seo

--- src/app/changelogs/metadata.ts
+++ src/app/changelogs/metadata.ts
@@ -1,6 +1,7 @@
 import { Metadata } from 'next';
 
 export const defaultMetadata: Metadata = {
+  metadataBase: new URL('https://jailbreakchangelogs.xyz'),
   title: 'Jailbreak Changelogs & Update History',
   description: 'Track every update, feature release, and game modification in Jailbreak\'s history. Browse through our comprehensive collection of Roblox Jailbreak changelogs.',
   openGraph: {
@@ -8,10 +9,20 @@ export const defaultMetadata: Metadata = {
     description: 'Track every update, feature release, and game modification in Jailbreak\'s history. Browse through our comprehensive collection of Roblox Jailbreak changelogs.',
     type: 'website',
     siteName: 'Jailbreak Changelogs',
+    url: '/changelogs',
+    images: [
+      {
+        url: "https://assets.jailbreakchangelogs.xyz/assets/logos/JBCL_Long_Dark_Background.webp",
+        width: 1200,
+        height: 630,
+        alt: "Jailbreak Changelogs Banner",
+      },
+    ],
   },
   twitter: {
     card: 'summary_large_image',
     title: 'Jailbreak Changelogs & Update History',
     description: 'Track every update, feature release, and game modification in Jailbreak\'s history. Browse through our comprehensive collection of Roblox Jailbreak changelogs.',
+    images: ["https://assets.jailbreakchangelogs.xyz/assets/logos/JBCL_Long_Dark_Background.webp"],
   },
 }; 
