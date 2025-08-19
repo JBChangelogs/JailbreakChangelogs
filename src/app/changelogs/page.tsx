88b3e266fe1dfac41d13ab86aaa4e21f96701b48
Jalen <joe16moses@gmail.com>
2025-08-19 17:56:43 +0300

    fix

--- src/app/changelogs/page.tsx
+++ src/app/changelogs/page.tsx
@@ -4,7 +4,7 @@ import { PUBLIC_API_URL } from "@/utils/api";
 export const dynamic = "force-dynamic";
 
 export default async function ChangelogsPage() {
-  const response = await fetch(`${PUBLIC_API_URL}/changelogs/latest`, { cache: 'no-store' });
+  const response = await fetch(`${PUBLIC_API_URL}/changelogs/latest`);
   const data = await response.json();
   
   redirect(`/changelogs/${data.id}`);
