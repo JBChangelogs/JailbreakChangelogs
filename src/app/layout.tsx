e6d29a318dbbae953dbb3178bef67d6600e50e0b
Jalen <126164913+Jalenzzz@users.noreply.github.com>
2025-10-11 17:00:18 +0300

    refactor: simplify Clarity script loading in RootLayout by replacing inline script with a direct src attribute; enhances readability and maintainability

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -158,20 +158,10 @@ export default async function RootLayout({
               </ThemeProvider>
             </CustomThemeProvider>
           </AppRouterCacheProvider>
-          <Script id="clarity-script" strategy="afterInteractive">
-            {`
-              (function(c,l,a,r,i,t,y){
-                  c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-                  t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-                  y=l.getElementsByTagName(r)[0];
-                  if(y && y.parentNode && y.parentNode.contains(y)) {
-                      y.parentNode.insertBefore(t,y);
-                  } else {
-                      l.head.appendChild(t);
-                  }
-              })(window, document, "clarity", "script", "${process.env.NEXT_PUBLIC_CLARITY_PROJECT}");
-            `}
-          </Script>
+          <Script
+            id="clarity-script"
+            src={`https://www.clarity.ms/tag/${process.env.NEXT_PUBLIC_CLARITY_PROJECT}`}
+          />
         </body>
       </html>
     );
@@ -301,20 +291,10 @@ export default async function RootLayout({
             </ThemeProvider>
           </CustomThemeProvider>
         </AppRouterCacheProvider>
-        <Script id="clarity-script" strategy="afterInteractive">
-          {`
-            (function(c,l,a,r,i,t,y){
-                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-                y=l.getElementsByTagName(r)[0];
-                if(y && y.parentNode && y.parentNode.contains(y)) {
-                    y.parentNode.insertBefore(t,y);
-                } else {
-                    l.head.appendChild(t);
-                }
-            })(window, document, "clarity", "script", "${process.env.NEXT_PUBLIC_CLARITY_PROJECT}");
-          `}
-        </Script>
+        <Script
+          id="clarity-script"
+          src={`https://www.clarity.ms/tag/${process.env.NEXT_PUBLIC_CLARITY_PROJECT}`}
+        />
       </body>
     </html>
   );
