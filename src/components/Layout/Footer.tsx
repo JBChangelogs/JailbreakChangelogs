0b3b2fde6a7c2d2483b67130e7cba3dfd40d8c0e
Jalen <126164913+Jalenzzz@users.noreply.github.com>
2025-10-31 03:53:45 +0300

    chore(Footer): remove unused Image import

--- src/components/Layout/Footer.tsx
+++ src/components/Layout/Footer.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import Link from "next/link";
-import Image from "next/image";
 import dynamic from "next/dynamic";
 import { Suspense } from "react";
 import { Icon } from "../UI/IconWrapper";
