ca5777483bfd96d090e0b6a73abfea820d576ce6
Jalen <126164913+Jalenzzz@users.noreply.github.com>
2025-12-24 15:33:46 +0300

    chore: remove Sentry integration and related configurations

--- next.config.js
+++ next.config.js
@@ -105,36 +105,4 @@ const nextConfig = {
   },
 };
 
-// Injected content via Sentry wizard below
-import { withSentryConfig } from "@sentry/nextjs";
-
-export default withSentryConfig(nextConfig, {
-  // For all available options, see:
-  // https://www.npmjs.com/package/@sentry/webpack-plugin#options
-
-  org: "jailbreak-changelogs",
-  project: "jailbreak_changelogs_prod",
-
-  // Only print logs for uploading source maps in CI
-  silent: !process.env.CI,
-
-  // For all available options, see:
-  // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
-
-  // Upload a larger set of source maps for prettier stack traces (increases build time)
-  widenClientFileUpload: true,
-
-  // Route browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers.
-  // This can increase your server load as well as your hosting bill.
-  // Note: Check that the configured route will not match with your Next.js middleware, otherwise reporting of client-
-  // side errors will fail.
-  // tunnelRoute: '/monitoring', // DISABLED to reduce server usage
-
-  // Automatically tree-shake Sentry logger statements to reduce bundle size
-  webpack: {
-    treeshake: {
-      removeDebugLogging: true,
-    },
-    automaticVercelMonitors: true,
-  },
-});
+export default nextConfig;
