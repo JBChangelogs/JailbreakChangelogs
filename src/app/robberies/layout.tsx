daddde78f4c603607ce3cf167ac703ae970a6b12
Jalen <126164913+Jalenzzz@users.noreply.github.com>
2026-01-01 17:44:04 +0300

    fix: update image URLs to use old logo

--- src/app/robberies/layout.tsx
+++ src/app/robberies/layout.tsx
@@ -13,7 +13,7 @@ export const metadata: Metadata = {
       "Track live status of robberies, mansions, and airdrops in Roblox Jailbreak. Get real-time updates on robberies, mansions, and airdrops.",
     images: [
       {
-        url: "https://assets.jailbreakchangelogs.xyz/assets/logos/JBCL_Short_Christmas_Background.png",
+        url: "https://assets.jailbreakchangelogs.xyz/assets/logos/JBCL_Long_Light_Background.png",
         width: 1200,
         height: 630,
         alt: "Jailbreak Changelogs Banner",
@@ -29,7 +29,7 @@ export const metadata: Metadata = {
     description:
       "Track live status of robberies, mansions, and airdrops in Roblox Jailbreak. Get real-time updates on robberies, mansions, and airdrops.",
     images: [
-      "https://assets.jailbreakchangelogs.xyz/assets/logos/JBCL_Short_Christmas_Background.png",
+      "https://assets.jailbreakchangelogs.xyz/assets/logos/JBCL_Long_Light_Background.png",
     ],
   },
 };
