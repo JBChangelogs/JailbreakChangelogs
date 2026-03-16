8e31b7fbbabf75004f6f8ee088bc6729cc89126f
KAS <65353028+dev-kas@users.noreply.github.com>
2026-03-17 02:45:08 +0530

    chore: remove redundant allowed paths from robots rules
    Removed allowed paths from robots.txt configuration since they are already allowed by default and no disallow list is blocking them.
    
    Signed-off-by: KAS <65353028+dev-kas@users.noreply.github.com>

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -5,7 +5,6 @@ export default function robots(): MetadataRoute.Robots {
     rules: [
       {
         userAgent: "*",
-        allow: ["/values", "/values/*", "/item/*", "/changelogs", "/seasons"],
         disallow: [
           "/*?page=",
           "/*?filter=",
