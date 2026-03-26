af27c2975a0fc76ea54ba0f4a5d26b4eff1f03cf
Jalen <126164913+Jalenzzz@users.noreply.github.com>
2026-03-26 05:20:16 +0300

    feat(config): add new proxy and asset hostnames for Umami analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -99,7 +99,7 @@ export default async function RootLayout({
           {/* Umami Analytics */}
           <Script
             defer
-            src={`https://umami.jailbreakchangelogs.xyz${
+            src={`https://umami.jailbreakchangelogs.com${
               process.env.NEXT_PUBLIC_UMAMI_SCRIPT_NAME
                 ? process.env.NEXT_PUBLIC_UMAMI_SCRIPT_NAME.startsWith("/")
                   ? process.env.NEXT_PUBLIC_UMAMI_SCRIPT_NAME
@@ -243,7 +243,7 @@ export default async function RootLayout({
         {/* Umami Analytics */}
         <Script
           defer
-          src={`https://umami.jailbreakchangelogs.xyz${
+          src={`https://umami.jailbreakchangelogs.com${
             process.env.NEXT_PUBLIC_UMAMI_SCRIPT_NAME
               ? process.env.NEXT_PUBLIC_UMAMI_SCRIPT_NAME.startsWith("/")
                 ? process.env.NEXT_PUBLIC_UMAMI_SCRIPT_NAME
