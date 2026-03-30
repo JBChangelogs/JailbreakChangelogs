f1f1a1d8e85d71b652a3e584dcac6e48e764c8dd
Jalen <126164913+Jalenzzz@users.noreply.github.com>
2026-03-30 19:26:08 +0300

    chore: remove deprecated ads.txt file and implement new redirect route for ads

--- src/app/ads.txt/route.ts
+++ src/app/ads.txt/route.ts
@@ -1,6 +1,6 @@
 import { NextResponse } from "next/server";
 
-const NITROPAY_ADS_URL = "https://api.nitropay.com/v1/ads-1148.txt";
+const NITROPAY_ADS_URL = "https://api.nitropay.com/v1/ads-2263.txt";
 
 export function GET() {
   return NextResponse.redirect(NITROPAY_ADS_URL, 301);
