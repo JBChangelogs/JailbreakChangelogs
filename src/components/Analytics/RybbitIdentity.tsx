966437a1a45eddcfdefb151d191f5cb5c0ccfbde
Jalen <126164913+Jalenzzz@users.noreply.github.com>
2026-05-22 20:55:08 +0300

    feat(analytics): send resolved avatar url as rybbit identify trait

--- src/components/Analytics/RybbitIdentity.tsx
+++ src/components/Analytics/RybbitIdentity.tsx
@@ -27,6 +27,21 @@ export default function RybbitIdentity() {
         traits.roblox_username = user.roblox_username;
       }
 
+      const useDiscord = user.settings?.avatar_discord === 1;
+      const useCustom =
+        user.settings?.avatar_discord === 0 &&
+        user.premiumtype >= 2 &&
+        user.custom_avatar &&
+        user.custom_avatar !== "N/A";
+
+      if (useCustom && user.custom_avatar) {
+        traits.avatar = user.custom_avatar;
+      } else if (useDiscord && user.avatar && user.avatar !== "None") {
+        traits.avatar = `https://cdn.discordapp.com/avatars/${user.id}/${user.avatar}?size=256`;
+      } else if (user.avatar && user.avatar !== "None") {
+        traits.avatar = `https://cdn.discordapp.com/avatars/${user.id}/${user.avatar}?size=256`;
+      }
+
       trackIdentify(user.id, traits);
       return true;
     };
