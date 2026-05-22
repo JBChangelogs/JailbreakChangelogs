53b20bc5fd000fe3c1c0716bad07840fc4e9e209
AvengerDCFan13YT <132626613+AvengerDCFan13YT@users.noreply.github.com>
2026-05-22 13:20:47 -0500

    Update navbar.tsx

--- src/components/ui/navbar.tsx
+++ src/components/ui/navbar.tsx
@@ -608,7 +608,7 @@ export const NavbarModern = ({
             <Image
               src={
                 isCollabPage
-                  ? `/logos/collab/JBCL_X_TC_Logo_Long_Transparent_${resolvedTheme === "og" ? "Dark" : "Light"}.webp`
+                  ? `/logos/collab/JBCL_X_TC_Logo_Long_Transparent_${resolvedTheme === "dark" ? "Dark" : "Light"}.webp`
                   : resolvedTheme === "og"
                     ? "/logos/OLD/JBCL_Long_Transparent.webp"
                     : "/logos/JBCL_Long_Transparent.webp"
