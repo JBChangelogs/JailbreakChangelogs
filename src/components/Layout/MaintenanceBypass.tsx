b229f9947e4f5dd6e09d3f5630d0735d1cc0fb96
Jalen <126164913+Jalenzzz@users.noreply.github.com>
2026-05-23 15:45:31 +0300

    fix(maintenance): remove header from maintenance page

--- src/components/Layout/MaintenanceBypass.tsx
+++ src/components/Layout/MaintenanceBypass.tsx
@@ -1,8 +1,6 @@
 "use client";
 
 import Maintenance from "@/theme/Maintenance";
-import Header from "./Header";
-import { AuthProvider } from "@/contexts/AuthContext";
 
 interface MaintenanceBypassProps {
   children: React.ReactNode;
@@ -11,12 +9,5 @@ interface MaintenanceBypassProps {
 export default function MaintenanceBypass({
   children: _children,
 }: MaintenanceBypassProps) {
-  return (
-    <div className="flex min-h-screen flex-col">
-      <AuthProvider>
-        <Header />
-        <Maintenance />
-      </AuthProvider>
-    </div>
-  );
+  return <Maintenance />;
 }
